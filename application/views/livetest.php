1126208265700c967e5d2a954a0d0f642464af68
arlpple9 <62857719+arlpple9@users.noreply.github.com>
2022-06-27 23:44:44 +0800

    css initial

--- application/views/livetest.php
+++ application/views/livetest.php
@@ -8,47 +8,59 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap%27');
       body, html{
 	    font-family: 'Poppins', sans-serif;
       }
       :root {
-        --light-color: #D9E9E8!important;
-        --dark-color: #220e5d!important;
+        --light-color: #F3F8F8;
+        --dark-main-color: #220e5d;
+        --black-color: #0c0c0c;
 
-        --main-color: #5426de!important;
-        --sec-color: #94abef!important;
+        --main-color: #5426de;
+        --sec-color: #94abef;
+      }
+      .dnone{display:none;}
+      .navbar-brand img{
+        height: 48px; !important;
       }
     </style>
 
     <title>Safety Pal</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-md navbar-light">
-        <div id="navbar_logo_container">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="https://source.unsplash.com/600x900/?meow" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> Safety Pal</a>
+    <nav class="navbar navbar-expand-md">
+      <div class="container">
+        <div class="navbar-brand">
+          <img src="assets/imgs/logo_safetypal.png">
         </div>
-    
         <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="#">HOME</a>
+              <a class="nav-link" href="#">link 1</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">ABOUT US</a>
+              <a class="nav-link" href="#">link 2</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">CONTACT US</a>
+              <a class="nav-link" href="#">link 3</a>
             </li>
-        </ul>
+          </ul>
         </div>
-
-        <div id="testingasd" class="text-muted">asdasdasd</div>
+        <button class="button-download-nav">Download Apk</button>
+        <i class="fa fa-bars fa-inverse dnone" aria-hidden="true"></i>
+      </div>
     </nav>
+
+  
+
+    <div class="card">
+      <img src=https://source.unsplash.com/600x900/?crypto" class="w-100">
+      <div class="card-img-overlay"></div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
